9464f8c9500c914d34fc2efbec5ed8ee1b096c03
Long <khoathanglong@gmail.com>
2020-02-04 15:14:56 +0100

    update timeline

--- src/timeline.js
+++ src/timeline.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-destructuring */
 /* eslint-disable consistent-return */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable prefer-arrow-callback */
@@ -17,6 +18,42 @@ const icons = {
   pinned:
     'M52.963,21.297c-0.068-0.329-0.297-0.603-0.609-0.727c-2.752-1.097-5.67-1.653-8.673-1.653  c-4.681,0-8.293,1.338-9.688,1.942L19.114,8.2c0.52-4.568-1.944-7.692-2.054-7.828C16.881,0.151,16.618,0.016,16.335,0  c-0.282-0.006-0.561,0.091-0.761,0.292L0.32,15.546c-0.202,0.201-0.308,0.479-0.291,0.765c0.016,0.284,0.153,0.549,0.376,0.726  c2.181,1.73,4.843,2.094,6.691,2.094c0.412,0,0.764-0.019,1.033-0.04l12.722,14.954c-0.868,2.23-3.52,10.27-0.307,18.337  c0.124,0.313,0.397,0.541,0.727,0.609c0.067,0.014,0.135,0.021,0.202,0.021c0.263,0,0.518-0.104,0.707-0.293l14.57-14.57  l13.57,13.57c0.195,0.195,0.451,0.293,0.707,0.293s0.512-0.098,0.707-0.293c0.391-0.391,0.391-1.023,0-1.414l-13.57-13.57  l14.527-14.528C52.929,21.969,53.031,21.627,52.963,21.297z',
 };
+
+function generateColor(color) {
+  return [
+    d3.hsl(color).darker(0.5),
+    d3.hsl(color).darker(0.3),
+    color,
+    d3.hsl(color).brighter(0.5),
+    d3.hsl(color).brighter(1),
+  ];
+}
+
+function drawRect(
+  container,
+  x,
+  y,
+  rx,
+  ry,
+  width,
+  height,
+  color = null,
+  stroke = null,
+  classed = null,
+) {
+  const rectContainer = d3.select(container);
+  rectContainer
+    .append('rect')
+    .attr('class', classed)
+    .attr('fill', color)
+    .attr('stroke', stroke)
+    .attr('x', x)
+    .attr('y', y)
+    .attr('rx', rx)
+    .attr('ry', ry)
+    .attr('width', width)
+    .attr('height', height);
+}
 class Timeline {
   constructor(chartContainer) {
     // chart variables
@@ -74,6 +111,13 @@ class Timeline {
     this.originalData = [];
     //
     this.updateData = this.updateData.bind(this);
+    this.onResize = this.onResize.bind(this);
+    this.throttleOnResize = _.throttle(this.onResize, 500);
+    window.addEventListener(
+      'resize',
+      this.throttleOnResize,
+      `${this.chartContainer}`,
+    );
     this.init();
   }
 
@@ -87,7 +131,6 @@ class Timeline {
     this.implementAxisSelection();
     this.implementExpandingAll();
     this.implementTooltip();
-    this.implementColorPicker();
 
     this.svg = d3
       .select(`#${this.chartContainer} .profileTimeline`)
@@ -101,8 +144,8 @@ class Timeline {
       .append('svg:clipPath')
       .attr('id', 'clip')
       .append('svg:rect')
-      .attr('width', 10)
-      .attr('height', 10)
+      .attr('width', 20)
+      .attr('height', 20)
       .attr('x', -this.r)
       .attr('y', -this.r);
     // create day xAxis
@@ -121,29 +164,19 @@ class Timeline {
     d3.select(`#${this.chartContainer} .profileTimeline`).on('click', () => {
       const tagName = d3.event.target.tagName.toLowerCase();
       const clickOnCircle = typeof tagName === 'string' && tagName === 'circle';
-      const colorPicker = d3
-        .select(`#${this.chartContainer}`)
-        .select('.colorPicker');
 
       if (!clickOnCircle) {
-        colorPicker
-          .transition()
-          .duration(400)
-          .style('opacity', 0)
-          .style('z-index', -1);
+        d3.select(`g#${this.chartContainer}-paletteContainer`).remove();
       }
     });
+  }
 
-    window.addEventListener(
-      'resize',
-      _.throttle(() => {
-        this.width = document.getElementById(this.chartContainer).offsetWidth
-          - this.margin.left
-          - this.margin.right;
-        this.updateScale();
-        this.drawTimeline(this.originalData);
-      }, 200),
-    );
+  onResize() {
+    this.width = document.getElementById(this.chartContainer).offsetWidth
+      - this.margin.left
+      - this.margin.right;
+    this.updateScale();
+    this.drawTimeline(this.originalData);
   }
 
   updateData(rawData) {
@@ -354,51 +387,136 @@ class Timeline {
       .style('opacity', 0);
   }
 
-  implementColorPicker() {
-    const container = d3
-      .select(`#${this.chartContainer} .profileTimeline`)
-      .append('div')
-      .attr('class', 'colorPicker selected-color')
-      .style('opacity', 0)
-      .style('z-index', -1);
-
-    const palette = [
-      '#1f78b4',
-      '#33a02c',
-      '#e31a1c',
-      '#ff7f00',
-      '#6a3d9a',
-      '#b15928',
+  drawPalette(x = 0, y = 0) {
+    // this color palette is inspired and allowed to use by Adnen rebai
+    // orginal chart https://bl.ocks.org/adnenre/8fae80314887a4554db99268c1d7041e
+    const colors = [
+      '#ff4422',
+      '#ee1166',
+      '#9911bb',
+      '#6633bb',
+      '#3344bb',
+      '#1199ff',
+      '#00aaff',
+      '#00bbdd',
+      '#009988',
+      '#44bb44',
+      '#88cc44',
+      '#ccdd22',
+      '#ffee11',
+      '#ffcc00',
+      '#ff9900',
+      '#ff5500',
+      '#775544',
+      '#999999',
+      '#828080',
+      '#444',
     ];
+    const rectRadius = 4;
+    const rectLength = 25;
+    const rectGap = 35;
+    const rectHolderLength = 200;
+    const rectHolderRadius = 5;
+    const svg = d3.select(`#${this.chartContainer} .timelineChart`);
 
-    const iconColors = [
-      '#a6cee3',
-      '#b2df8a',
-      '#fb9a99',
-      '#fdbf6f',
-      '#cab2d6',
-      '#ff9',
-    ];
-    const colorButtons = container
-      .selectAll('button')
-      .data(palette)
-      .enter()
-      .append('button');
+    const paletteContainer = svg
+      .append('g')
+      .attr('id', `${this.chartContainer}-paletteContainer`)
+      .style('position', 'relative')
+      .style('z-index', 20);
 
-    colorButtons
-      .attr('title', 'Set selected events color')
-      .attr('class', 'btn selected-color btn-sm')
-      .attr(
-        'style',
-        (d, i) => `background: ${d}; borderColor: ${iconColors[i]}`,
+    paletteContainer.attr('transform', `scale(0.5) translate(${x},${y})`);
+
+    for (let i = 0; i < colors.length; i += 1) {
+      paletteContainer
+        .append('g')
+        .attr('id', `${this.chartContainer}-rectContainer${i}`)
+        .attr('data-color', colors[i])
+        .attr('class', 'rectContainer')
+        .attr('style', `transform-origin: ${x}px ${y + 10}px`);
+
+      drawRect(
+        `#${this.chartContainer}-rectContainer${i}`,
+        x,
+        y,
+        rectHolderRadius,
+        rectHolderRadius,
+        rectHolderLength,
+        rectLength,
+        '#fff',
+        '#ccc',
+      );
+      drawRect(
+        `#${this.chartContainer}-rectContainer${i}`,
+        x + rectGap,
+        y,
+        rectRadius,
+        rectRadius,
+        rectLength,
+        rectLength,
+        generateColor(colors[i])[0],
+        '',
+        'color_rect color_rect0',
+      );
+      drawRect(
+        `#${this.chartContainer}-rectContainer${i}`,
+        x + rectGap * 2,
+        y,
+        rectRadius,
+        rectRadius,
+        rectLength,
+        rectLength,
+        generateColor(colors[i])[1],
+        '',
+        'color_rect color_rect1',
+      );
+      drawRect(
+        `#${this.chartContainer}-rectContainer${i}`,
+        x + rectGap * 3,
+        y,
+        rectRadius,
+        rectRadius,
+        rectLength,
+        rectLength,
+        generateColor(colors[i])[2],
+        '',
+        'color_rect color_rect2',
+      );
+      drawRect(
+        `#${this.chartContainer}-rectContainer${i}`,
+        x + rectGap * 4,
+        y,
+        rectRadius,
+        rectRadius,
+        rectLength,
+        rectLength,
+        generateColor(colors[i])[3],
+        '',
+        'color_rect color_rect3',
+      );
+      drawRect(
+        `#${this.chartContainer}-rectContainer${i}`,
+        x + rectGap * 5,
+        y,
+        rectRadius,
+        rectRadius,
+        rectLength,
+        rectLength,
+        generateColor(colors[i])[4],
+        '',
+        'color_rect color_rect4',
       );
 
-    colorButtons
-      .append('span')
-      .attr('class', 'fa fa-paint-brush selected-color')
-      .attr('style', (d, i) => `color: ${iconColors[i]}`);
+      const step = 18;
 
-    this.colorButtons = colorButtons;
+      // eslint-disable-next-line no-shadow
+      for (let i = 0; i < colors.length; i += 1) {
+        d3.select(`#${this.chartContainer}-rectContainer${i}`)
+          .transition()
+          .duration(500)
+          .attr('transform', `rotate(${i * step} )`);
+      }
+    }
   }
 
   expandDomain(domain) {
@@ -441,7 +559,6 @@ class Timeline {
     const existingElement = this.originalData.filter(
       el => el.belongTo === domain.label && el.isPinned,
     );
-    // _.remove(this.originalData, el => el.belongTo === domain.label && !el.isPinned);
     this.originalData.splice(
       domainIndex + 1,
       closingLength,
@@ -520,9 +637,9 @@ class Timeline {
     this.brush.call(this.brushed);
   }
 
-  drawTimeline(chartData) {
+  drawTimeline(chartData, status) {
     // reset brushed if exists
-    if (this.brushed) {
+    if (this.brushed && status !== 'notResetBrush') {
       this.resetBrush();
     }
     // (re)calculate height and width
@@ -608,10 +725,15 @@ class Timeline {
     labelContainers
       .append('text')
       .attr('class', 'label')
-      .attr(
-        'style',
-        d => `font-size: ${!d.belongTo ? this.domainFontSize : this.fontSize}`,
-      )
+      .attr('style', (d) => {
+        let fontSize;
+        if (!d.belongTo) {
+          fontSize = this.domainFontSize;
+        } else {
+          fontSize = this.fontSize;
+        }
+        return `font-size: ${fontSize}`;
+      })
       .attr('fill', this.textFill)
       .style('text-anchor', 'start')
       .attr('x', 10);
@@ -675,7 +797,7 @@ class Timeline {
           selectedColor: d.selectedColor,
         }));
       },
-      d => d.startDay + d.endDay + d.conceptId + d.selectedColor,
+      d => d.startDay + d.endDay + d.conceptId + d.conceptName + d.selectedColor,
     );
 
     lines.exit().remove();
@@ -687,14 +809,21 @@ class Timeline {
       .attr('x1', d => this.xDayScale(d.startDay))
       .attr('x2', d => this.xDayScale(d.endDay))
       .attr('stroke', (d) => {
+        let filledColor;
         if (d.inDomainLine) {
-          return ` ${this.circleFill}`;
-        } if (d.selectedColor) {
-          return `${d.selectedColor}`;
+          filledColor = this.circleFill;
+        } else if (d.selectedColor) {
+          filledColor = d.selectedColor;
+        } else {
+          filledColor = this.colorScheme(d.conceptId);
         }
-        return `${this.colorScheme(d.conceptId)}`;
+
+        return filledColor;
       })
-      .attr('stroke-width', this.lineStrokeWidth);
+      .attr('stroke-width', (d) => {
+        const strokeWidth = this.lineStrokeWidth;
+        return strokeWidth;
+      });
 
     // cicles
     const circles = timelineChildren.selectAll('circle').data(
@@ -705,41 +834,86 @@ class Timeline {
           selectedColor: d.selectedColor,
         }));
       },
-      d => d.startDay + d.endDay + d.conceptId + d.selectedColor,
+      d => d.startDay + d.endDay + d.conceptId + d.conceptName + d.selectedColor,
     );
     circles.exit().remove();
-
+    const self = this;
     circles
       .enter()
       .append('circle')
       .attr('cx', d => this.xDayScale(d.startDay))
       .attr('style', (d) => {
+        let filledColor;
         if (d.inDomainLine) {
-          return `fill: ${this.circleFill}`;
-        } if (d.selectedColor) {
-          return `fill: ${d.selectedColor}`;
+          filledColor = this.circleFill;
+        } else if (d.selectedColor) {
+          filledColor = d.selectedColor;
+        } else {
+          filledColor = this.colorScheme(d.conceptId);
         }
-        return `fill: ${this.colorScheme(d.conceptId)}`;
+        return `fill: ${filledColor}`;
       })
       .attr('r', this.r)
       .attr('width', 100)
       .attr('height', 100)
-      .on('mouseover', (d) => {
+      .on('mouseover', function (d) {
         // display tooltip
         const singleTimelineData = chartData.filter(el => (d.inDomainLine
           ? el.label === d.domain
           : el.label === d.conceptName && d.domain === el.belongTo))[0];
-        this.showTooltip(singleTimelineData, d);
+        self.showTooltip(singleTimelineData, d);
+
+        if (!d.inDomainLine) {
+          self.hightlightRows(this);
+          // this refer to element click event
+        }
       })
-      .on('mouseout', () => {
-        this.hideTooltip();
+      .on('mouseout', function (d) {
+        self.hideTooltip();
+
+        if (!d.inDomainLine) {
+          self.unHighlightRow(this);
+          // this refer to element click event
+        }
       })
       .on('click', (d) => {
         if (d.inDomainLine) return;
         // return click event if any
-        this.colorButtons.on('click', null);
+        d3.select(`#${this.chartContainer}-paletteContainer`).remove();
         this.selectColor(d);
       });
+  }
+
+  hightlightRows(context) {
+    const circleColor = d3
+      .select(context)
+      .attr('style')
+      .slice(6);
+    const timelineChildren = d3.select(context.parentNode);
+    timelineChildren.attr('clip-path', '');
+    const selectedCircles = timelineChildren.selectAll('circle');
+    const selectedLines = timelineChildren.selectAll('line');
+    const highlightColor = d3.hsl(circleColor).brighter(0.8);
+    selectedCircles.attr('style', `fill: ${highlightColor}`);
+    selectedCircles.attr('r', this.r + 1);
+    selectedLines.attr('stroke', highlightColor);
+    selectedLines.attr('stroke-width', this.lineStrokeWidth + 2);
+  }
+
+  unHighlightRow(context) {
+    const circleColor = d3
+      .select(context)
+      .attr('style')
+      .slice(6);
+    const timelineChildren = d3.select(context.parentNode);
+    timelineChildren.attr('clip-path', 'url(#clip)');
+    const selectedCircles = timelineChildren.selectAll('circle');
+    const selectedLines = timelineChildren.selectAll('line');
+    const highlightColor = d3.hsl(circleColor).darker(0.8);
+    selectedCircles.attr('style', `fill: ${highlightColor}`);
+    selectedCircles.attr('r', this.r);
+    selectedLines.attr('stroke', highlightColor);
+    selectedLines.attr('stroke-width', this.lineStrokeWidth);
   }
 
   pinLabel(d) {
@@ -751,7 +925,7 @@ class Timeline {
     );
     this.allData[allDataIndex].isPinned = d.isPinned;
     this.originalData[originalDataIndex].isPinned = d.isPinned;
-    this.drawTimeline(this.originalData);
+    this.drawTimeline(this.originalData, 'notResetBrush');
   }
 
   showTooltip(timeLineData, d, content) {
@@ -843,6 +1017,11 @@ class Timeline {
         const { endDay, startDay } = item;
         const startDate = item.startDate || new Date(moment(new Date()).add(startDay, 'days'));
         const endDate = item.startDate || new Date(moment(new Date()).add(endDay, 'days'));
+        const dataDescription = `<div style="margin-bottom:5px">
+          <strong>${conceptId}</strong> <br />
+          <span>${conceptName}</span> <br />
+          <span>Frequency: ${1} </span>
+        </div>`;
         const observationData = {
           endDay,
           startDay,
@@ -852,6 +1031,8 @@ class Timeline {
           conceptName,
           domain,
           selectedColor: null,
+          frequency: 1,
+          dataDescription,
         };
 
         const timeLineDomain = {
@@ -875,7 +1056,6 @@ class Timeline {
           selectedColor: null,
         };
 
-        // push timeline for domain
         const timeLineDomainIndex = accumulator.findIndex(
           el => el.id === domain,
         );
@@ -905,48 +1085,39 @@ class Timeline {
   }
 
   selectColor(d) {
-    const colorPicker = d3.select(`#${this.chartContainer} .colorPicker`);
-    const colorPickerSize = colorPicker.node().getBoundingClientRect();
     const coordinates = d3.mouse(this.svg.node());
+    const x = coordinates[0];
+    const y = coordinates[1];
 
-    colorPicker.style(
-      'left',
-      `${coordinates[0] + this.margin.left - colorPickerSize.width / 2}px`,
-    );
-    colorPicker.style(
-      'top',
-      `${coordinates[1] + this.margin.top + colorPickerSize.height * 1.5}px`,
-    );
-
-    colorPicker
-      .transition()
-      .duration(400)
-      .style('opacity', 1)
-      .style('z-index', 10);
-
-    this.colorButtons.on('click', (color) => {
-      colorPicker
-        .transition()
-        .duration(400)
-        .style('opacity', 0)
-        .style('z-index', -1);
-      this.fillColor(d, color);
+    this.drawPalette(x, y);
+    const colorPickers = d3
+      .select(`#${this.chartContainer}-paletteContainer`)
+      .selectAll('.color_rect');
+    const self = this;
+    colorPickers.on('click', function () {
+      const selectedColor = d3.select(this).attr('fill');
+      self.fillColor(d, selectedColor);
       // remove event listener after click
       // if not removed, each time click on circle, this will be invoked 1 more time -> not good
-      this.colorButtons.on('click', null);
+      colorPickers.on('click', null);
+      d3.select(`#${self.chartContainer}-paletteContainer`).remove();
     });
   }
 
   fillColor(d, selectedColor) {
     const originalDataIndex = this.originalData.findIndex(
-      el => el.belongTo === d.domain && d.conceptId === el.id,
+      el => el.belongTo === d.domain
+        && d.conceptId === el.id
+        && d.conceptName === el.label,
     );
     const allDataIndex = this.allData.findIndex(
-      el => el.belongTo === d.domain && d.conceptId === el.id,
+      el => el.belongTo === d.domain
+        && d.conceptId === el.id
+        && d.conceptName === el.label,
     );
     this.allData[allDataIndex].selectedColor = selectedColor;
     this.originalData[originalDataIndex].selectedColor = selectedColor;
-    this.drawTimeline(this.originalData);
+    this.drawTimeline(this.originalData, 'notResetBrush');
   }
 
   remove() {
@@ -954,6 +1125,12 @@ class Timeline {
     this.allData = null;
     this.filterText = '';
     this.svg.selectAll('*').remove();
+    window.removeEventListener(
+      'resize',
+      this.throttleOnResize,
+      `${this.chartContainer}`,
+    );
+
     d3.select(`#${this.chartContainer}`)
       .selectAll('*')
       .remove();
